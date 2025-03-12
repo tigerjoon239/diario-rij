5ccdd50d3c380669726c0c42c3b2d5d0f114b9de
tigerjoon239 <alexander.lopezdelapaz1@gmail.com>
2025-03-11 22:05:43 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import DiarioMural from "./DiarioMural";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <DiarioMural />
+  </React.StrictMode>
+);
